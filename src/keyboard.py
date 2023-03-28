cb63654bf1306ce05a61572f09057a27a0c619e2
Vadim Andreev <vadimcola@mail.ru>
2023-03-28 22:19:37 +0700

    Реализованы два класса KeyBoard и MixinLang

---
+++ src/keyboard.py
@@ -0,0 +1,27 @@
+from src.item import Item
+
+
+class MixinLang:
+    def __init__(self, language="EN"):
+        self.__language = language
+
+    def change_lang(self):
+        if self.__language == "EN":
+            self.__language = "RU"
+            return self
+        elif self.__language == "RU":
+            self.__language = "EN"
+            return self
+
+    @property
+    def language(self):
+        return self.__language
+
+
+class KeyBoard(Item, MixinLang):
+    def __init__(self, name, price, quantity, language="EN"):
+        super().__init__(name, price, quantity)
+        self.__language = language
+
+
+
